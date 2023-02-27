a82168864796a13b6a901dbcc68eb98949034431
deshdeepak <deshdeepakmohanpur@gmail.com>
2023-02-27 14:28:46 +0530

    Final

---
+++ src/main/java/com/example/Book_My_Show_Application/Entities/TheaterSeatEntity.java
@@ -0,0 +1,31 @@
+package com.example.Book_My_Show_Application.Entities;
+
+
+import com.example.Book_My_Show_Application.Enums.SeatType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "theater_seats")
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class TheaterSeatEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private SeatType seatType;
+
+    private String seatNo;
+
+    @ManyToOne
+    @JoinColumn
+    private TheaterEntity theaterEntity;
+
+}
